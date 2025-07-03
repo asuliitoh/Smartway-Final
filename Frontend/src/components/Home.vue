944e831eb921457483c5bc4c720dc610283f651b
jesus <jesussanchezz0004@gmail.com>
2025-07-03 11:33:00 +0200

    Diseño básico de página home hecha

--- Frontend/src/components/Home.vue
+++ Frontend/src/components/Home.vue
@@ -7,26 +7,124 @@ import Header from './Header.vue';
 </script>
 
 <template>
-    <div class="flex flex-col flex-1 min-w-screen min-h-screen bg-[radial-gradient(circle,_rgba(255,_252,_252,_1)_0%,_rgba(255,_227,_255,_1)_100%)]">
-        <Header class=" backdrop-blur-lg flex"></Header>
-        <div class="flex flex-1 justify-center items-center">
-            
-            <div class="shadow-sm">
-                <h1 class="text-4xl font-bold">Bienvenido, Agente número 000</h1>
-            </div>
-            
-            
-        </div>
-        
-        
+    <div class="pl-10 pr-10 flex flex-col min-h-screen bg-base-300">
+        <Header class="backdrop-blur-lg flex"></Header>
+        <div class="flex-1 flex justify-center items-center">
 
+            <div class="grid auto-rows-2 grid-cols-2 gap-10">
+            
+                <section class="row-start-1 col-start-1 card p-3 shadow-xl bg-base-200">
+                    <div class="card-body">
+                        <h1 class="card-title text-4xl font-bold tracking-tighter">Bienvenido a CyberPulse Labs</h1>
+                        <p class="pt-3 text-lg tracking-tighter ">Gestiona tus operaciones de manera <span class="text-primary font-semibold">eficiente</span> y gestiona tus equipos con otros agentes</p>
+                        <div class="pt-5 grid grid-cols-3 gap-5">
+
+                            <div class="col-start-1 card shadow-xl bg-base-100">
+                                <div class="card-body justify-center items-center">
+                                    <h2 class="card-title"> 0 </h2>
+                                    <p class="text-xs">Agentes Activos</p>
+                                </div>
+                            </div>
+
+                            <div class="col-start-2 card shadow-xl bg-base-100">
+                                <div class="card-body justify-center items-center">
+                                    <h2 class="card-title"> 0 </h2>
+                                    <p class="text-xs">Equipos Formados</p>
+                                </div>
+                            </div>
+
+                            <div class="col-start-3 card shadow-xl bg-base-100">
+                                <div class="card-body justify-center items-center">
+                                    <h2 class="card-title"> +0 </h2>
+                                    <p class="text-xs">Operaciones Completadas</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            
+                <section class="row-start-2 col-start-1 card p-3 shadow-xl bg-base-200">
+                    <div class="card-body">
+                        <h1 class="card-title text-4xl font-bold tracking-tighter">Gestionar agentes</h1>
+                        <p class="pt-3 text-lg tracking-tighter">Actualiza periódicamente tu información profesional y credenciales para reflejar con precisión tu trayectoria, reforzar tu credibilidad y abrir nuevas oportunidades de negocio.</p>
+                    </div>
+                    <div class="card-actions justify-end">
+                         <button class="btn">Buscar Agente</button>
+                         <button class="btn btn-primary">Editar perfil de agente</button>
+                    </div>
+                </section>
+
+                <section class="row-start-1 col-start-2 card p-3 shadow-xl bg-base-200">
+                    <div class="card-body">
+                        <h1 class="card-title text-4xl font-bold tracking-tighter">Gestiona tus equipos</h1>
+                        <p class="pt-3 text-lg tracking-tighter ">Organiza y supervisa equipos de trabajo, sus miembros agentes y operaciones activas.</p>
+                        <div class="pt-5 grid grid-cols-2 gap-5">
+
+                            <div class="col-start-1 card shadow-xl bg-base-100">
+                                <div class="card-body justify-center items-center stat-card rounded-xl">
+                                    <h2 class="card-title"> 0 </h2>
+                                    <p class="text-xs">Equipos propios</p>
+                                </div>
+                            </div>
+
+                            <div class="col-start-2 card shadow-xl bg-base-100">
+                                <div class="card-body justify-center items-center">
+                                    <h2 class="card-title"> 0 </h2>
+                                    <p class="text-xs">Equipos ajenos</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-actions justify-end">
+                         <button class="btn">Ver Equipos</button>
+                         <button class="btn btn-primary">Gestionar Equipos</button>
+                    </div>
+                </section>
+
+                <section class="row-start-2 col-start-2 card p-3 shadow-xl bg-base-200">
+                    <div class="card-body">
+                        <h1 class="card-title text-4xl font-bold tracking-tighter">Gestiona tus operaciones</h1>
+                        <p class="pt-3 text-lg tracking-tighter ">Gestiona todas las operaciones y controla aquellas operaciones en curso.</p>
+                        <div class="pt-5 grid grid-cols-3 gap-5">
+
+                            <div class="col-start-1 card shadow-xl bg-base-100">
+                                <div class="card-body justify-center items-center">
+                                    <h2 class="card-title"> 0 </h2>
+                                    <p class="text-xs">Operaciones Planificadas</p>
+                                </div>
+                            </div>
+
+                            <div class="col-start-2 card shadow-xl bg-base-100">
+                                <div class="card-body justify-center items-center">
+                                    <h2 class="card-title"> 0 </h2>
+                                    <p class="text-xs">Operaciones Activas</p>
+                                </div>
+                            </div>
+
+                            <div class="col-start-3 card shadow-xl bg-base-100">
+                                <div class="card-body justify-center items-center">
+                                    <h2 class="card-title"> 0 </h2>
+                                    <p class="text-xs">Operaciones Completadas</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="card-actions justify-end">
+                         <button class="btn">Ver Operaciones</button>
+                         <button class="btn btn-primary">Gestionar Operaciones</button>
+                    </div>
+                </section>
+        
+            </div>
+
+        </div>
 
     </div>
    
 </template>
 
 <style scoped>
-
 
 
 </style>
